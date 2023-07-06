5b036fa02b5f54495308dbd33a9384fdde1c737e
Vincent Kraus <mail@vinckr.com>
2023-07-06 15:06:38 +0200

    u

--- static/whoami.js
+++ static/whoami.js
@@ -1,16 +1,12 @@
-async function getSession() {
-  const url = "https://auth.slackinviter.vinckr.com/sessions/whoami";
+const url = "https://auth.slackinviter.vinckr.com/sessions/whoami";
 
-  try {
-    fetch(url, {
-      method: "GET",
-      credentials: "include",
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  } catch (error) {
-    console.error(error.message); // Error message
-  }
+try {
+  fetch(url, {
+    method: "GET",
+    credentials: "include",
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+} catch (error) {
+  console.error(error.message); // Error message
 }
-
-getSession();
